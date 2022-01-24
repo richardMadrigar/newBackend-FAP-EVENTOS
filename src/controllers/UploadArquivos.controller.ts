a32ff1ef6ca39c6a941c47171b20dc00f8397556
RichardMadrigar <richard_madrigar99@hotmail.com>
2022-01-24 20:14:29 -0300

    alter controllers de update img

--- src/controllers/UploadArquivos.controller.ts
+++ src/controllers/UploadArquivos.controller.ts
@@ -6,33 +6,13 @@ import fs from 'fs';
 import { pool } from '../config/configDataBase/database';
 import logger from '../config/configLogger';
 
-export const uploadArquivos = async (req: Request, res: Response) => {
-  const result = req.file;
-
-  if (!result) {
-    return res.status(400).json({ success: 'conteudo zerado', result });
-  }
-
-  const { filename, originalname } = result;
-
-  try {
-    await pool.query(`INSERT INTO uploads_doc (file_name, file_path, id_usuario, file_path_completed)
-    VALUES ($1, $2, $3, $4) `, [filename, `files/${originalname}`, '400', `http://localhost:3001/files/${filename}`]);
-
-    return res.status(200).json({ success: true, result });
-  } catch (error) {
-    logger.fatal(error);
-    return res.status(400).json({ success: false, result });
-  }
-};
-
 export const selectUserImg = async (req: Request, res: Response) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM uploads_doc ');
+    const { rows, rowCount } = await pool.query('SELECT * FROM img_perfil_usuarios ');
 
-    return res.status(201).json({
-      rows,
-    });
+    if (!rowCount) res.status(400).json('imagens não existe ');
+
+    return res.status(201).json({ rows });
   } catch (error) {
     logger.fatal(error);
     return res.status(500).json('Internal Server error');
@@ -45,14 +25,15 @@ export const deleteImg = async (req: Request, res: Response) => {
   const filePath = path.join(__dirname, '../../tmp/uploads');
 
   try {
-    fs.unlink(`${filePath}/${id}`, (err: any) => {
-      if (err) {
-        logger.error(err);
-      }
+    fs.unlink(`${filePath}/${id}`, (error: any) => {
+      if (error) logger.fatal(error);
     });
-    const resultId = await pool.query('DELETE FROM uploads_doc WHERE file_name = $1', [id]);
 
-    if (!resultId.rowCount) {
+    const { rowCount } = await pool.query(`
+    UPDATE img_perfil_usuarios SET img_perfil = $1 WHERE id_usuario = $2
+    `, [null, id]);
+
+    if (!rowCount) {
       return res.json(`Imagem não existe - ${id}`);
     }
 
@@ -64,7 +45,7 @@ export const deleteImg = async (req: Request, res: Response) => {
   }
 };
 
-export const updateImgUser = async (req: Request, res: Response): Promise<Response> => {
+export const updateImgUser = async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = req.file;
 
@@ -75,13 +56,15 @@ export const updateImgUser = async (req: Request, res: Response): Promise<Respon
   const { filename } = result;
 
   try {
-    const resultId = await pool.query(
-      `
-       UPDATE users SET img_perfil = $1  WHERE id = $2`,
-      [`http://localhost:3001/files/${filename}`, id],
+    const { rowCount } = await pool.query(
+      ` UPDATE img_perfil_usuarios  SET 
+      img_perfil = $1,
+      file_path = $2
+      WHERE id_usuario = $3`,
+      [`http://localhost:3001/files/${filename}`, filename, id],
     );
 
-    if (!resultId.rowCount) {
+    if (!rowCount) {
       return res.json(`Usuario com ${id} não existe `);
     }
 
